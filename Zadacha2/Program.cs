f5133317311827ad95a930efefe4336266e3e8f1
Student <Student@DESKTOP-FCRGMHM>
2024-02-19 10:03:30 +0200

    Add project files.

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Прочитане на входните данни
+            string[] stringArray = Console.ReadLine().Split();
+            List<string> commands = new List<string>();
+
+            // Обработка на командите
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                commands.Add(command);
+            }
+
+            // Обработка на входните данни и командите
+            ProcessCommands(stringArray, commands);
+
+            // Извеждане на резултата
+            Console.WriteLine(string.Join(", ", stringArray));
+        }
+
+        static void ProcessCommands(string[] array, List<string> commands)
+        {
+            foreach (var command in commands)
+            {
+                var parts = command.Split();
+                if (parts[0] == "Distinct")
+                {
+                    array = array.Distinct().ToArray();
+                }
+                else if (parts[0] == "Reverse")
+                {
+                    Array.Reverse(array);
+                }
+                else if (parts[0] == "Replace")
+                {
+                    int index = int.Parse(parts[1]) - 1;
+                    if (index >= 0 && index < array.Length)
+                    {
+                        array[index] = parts[2];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+            }
+        }
+    }
+}
---
+++ ZadachaGithub/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ZadachaGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Прочитане на входните данни
+            string[] stringArray = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            List<string> commands = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                commands.Add(Console.ReadLine());
+            }
+
+            // Обработка на командите
+            ProcessCommands(stringArray, commands);
+
+            // Извеждане на резултата
+            Console.WriteLine(string.Join(", ", stringArray));
+        }
+
+        static void ProcessCommands(string[] array, List<string> commands)
+        {
+            foreach (var command in commands)
+            {
+                if (command == "Reverse")
+                {
+                    Array.Reverse(array);
+                }
+                else if (command == "Distinct")
+                {
+                    array = array.Distinct().ToArray();
+                }
+                else if (command.StartsWith("Replace"))
+                {
+                    var parts = command.Split();
+                    int index = int.Parse(parts[1]);
+                    string newString = parts[2];
+                    array[index - 1] = newString;
+                }
+            }
+        }
+    }
+}
